bf059d373dbe69cdd5eb33969f64fa7a523f0155
Hank Taylor <hank@vercel.com>
2022-03-15 12:21:35 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
+<img src="Hank_Lets_Go.gif">
         </p>
 
         <div className={styles.grid}>
